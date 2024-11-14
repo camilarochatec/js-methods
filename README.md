cff1ea503bd15b8a3a95fe73b1193fa93d1b428b
camilarochatec <camilarocha.tec@gmail.com>
2024-11-13 22:37:20 -0300

    final

---
+++ README.md
@@ -0,0 +1,22 @@
+exercicios
+Atividades de Lógica com Array methods e String methods
+
+
+# Problema1:
+ Considere este array: let numbers = [1,2,3,4,5,6,7,8,9,10]; com base nele:
+-utilize uma função que retorne apenas os números ímpares;
+-com este resultado em mãos, retorne apenas os números divisíveis por 3
+
+# Problema2: 
+Considere este array 
+let words = [‘um’, ‘dois’, ‘tres’, ‘quatro’, ‘cinco’, ’seis’, ‘sete’. ‘oito’, ‘nove’, ‘dez’];
+ com base nele:
+-utilize alguma função para gerar um novo array onde os valores do array será o número de letras de cada palavra dentro do array original;
+-utilize alguma função para gerar um novo array com apenas as palavras que possuem encontros de vogais;
+-utilize alguma função para reordenar em ordem crescente este array com base no número de letras de cada palavra;
+
+# Problema3:
+ Considere esta variável: let data = ‘2024-01-03 13:10:00’;:
+-utilize alguma função para retornar a data formatada da seguinte maneira: 03/01/2023;
+-utilize alguma função para retornar apenas a hora: 13:10;
+
--- exercicios.js
+++ exercicios.js
@@ -1,21 +1,32 @@
-let impares = [1,2,3,4,5,6,7,8,9,10].filter(number => number % 2 != 0);
+//problema1
+let impares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter(number => number % 2 != 0);
 let divisiveisPorTres = impares.filter(number => number % 3 == 0);
 
-document.write(`<h1>Os valores impares do array numeros são: ${impares}</h1>`);
-document.write(`<h1>Os valores divisiveis por 3 são: ${divisiveisPorTres}</h1>`);
+document.write(`<h1>problema 1) <br> Os valores impares do array numeros são: ${impares}</h1>`);
+document.write(`<h1>problema 1b) <br>Os valores divisiveis por 3 são: ${divisiveisPorTres}</h1>`);
 
+//problema2
 let words = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
 
-document.write(`<h1>A qnt de letras de cada nome fica: ${
-    words.map(word => word.length)
-}</h1>`)
+document.write(`<h1>problema 2)<br>A qnt de letras de cada nome fica: ${words.map(word => word.length)
+    }</h1>`)
+// [aeiou]{2,}/i: Esta é a expressão regular que busca por duas ou mais vogais consecutivas (minúsculas ou maiúsculas) essa barras são usadas para procurar padrões dentro de uma string do ininico ao fim da string. 
+//A parte {2,} significa que estamos procurando por sequências de pelo menos 2 caracteres , Esse é um quantificador que indica que queremos 2 ou mais ocorrências consecutivas de qualquer um dos caracteres que definimos no conjunto , e o i no final da expressão regular torna a busca insensível ao caso (tanto faz se as vogais estão em maiúsculo ou minúsculo) O virgula após o número 2 indica que pode ser 2 ou mais (não há um limite superior especificado).
+// O .test() recebe uma string como argumento e verifica se essa string satisfaz a condição da expressão regular.
+// Ele retorna um valor booleano:
+// true: Se a expressão regular encontrar uma correspondência na string.
+// false: Se não houver correspondência.
+// filter(): Este método cria um novo array contendo apenas os elementos que satisfazem a condição definida pela função fornecida, que neste caso é a verificação da presença de encontros de vogais.
+document.write(`<h1>problema 2b)<br>as palavras que possuem encontros de vogais são: ${words.filter(word => /[aeiou]{2,}/i.test(word))}</h1>`)
 
-document.write(`<h1>O array ordenado fica: ${
-    words.map(word => word.length).sort()
-}</h1>`)
+document.write(`<h1>problema 2c)<br> O array ordenado crescente fica: ${words.map(word => word.length).sort()
+    }</h1>`)
 
-
+//problema3
 let data = '2024-11-13 11:35:00';
+
+document.write(`<h1>problema 3)<br> a data formatada é: ${data.split(' ')[0].split('-').join('/')}</h1>`)
+document.write(`<h1>problema 3b)<br> a hora é: ${data.split(' ')[1]}</h1>`)
 
 
 
